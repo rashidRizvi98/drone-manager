e1b643c58dc436e0da8c11b1a89a98a1f5acea9d
Rashid Ahamed <rashid123321a@gmail.com>
2023-05-06 01:36:23 +0530

    error handle in crate med

--- src/controllers/medication.ts
+++ src/controllers/medication.ts
@@ -6,16 +6,21 @@ import { getPreSignedUrl } from "../middlewares/file-upload";
 
 export const createMedication: RequestHandler = async (req,res,next) => {
     const payload : IMedication = req.body;
-    const [medication ,created] = await Medication.findOrCreate({
-        where: { code: payload.code },
-        defaults: {code: payload.code,name: payload.name,weight: payload.weight, image: (req?.file as any)?.location}
-    });
-    const responsePayload = {...medication?.dataValues,image: await getPreSignedUrl((req.file as any)?.key)}
-    if (created) {
-        return res.status(201)
-        .json({message: "Medication created Successfully", data: responsePayload});        
-    }else{
-        return res.status(200)
-        .json({message: "Medication has been already created", data: responsePayload});
+    try {
+        const [medication ,created] = await Medication.findOrCreate({
+            where: { code: payload.code },
+            defaults: {code: payload.code,name: payload.name,weight: payload.weight, image: (req?.file as any)?.key}
+        });
+        const responsePayload = {...medication?.dataValues,image: await getPreSignedUrl((req.file as any)?.key)}
+        if (created) {
+            return res.status(201)
+            .json({message: "Medication created Successfully", data: responsePayload});        
+        }else{
+            return res.status(200)
+            .json({message: "Medication has been already created", data: responsePayload});
+        }        
+    } catch (error) {
+        return next(error)
     }
+
 }
