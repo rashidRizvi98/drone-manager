201027edd4ee8aa2ba9499aca0c67e56eb136735
Rashid Ahamed <rashid123321a@gmail.com>
2023-05-06 21:53:56 +0530

    middleware to validate loading medication

---
+++ src/middlewares/load-medication.ts
@@ -0,0 +1,36 @@
+import { NextFunction, Request, Response } from "express";
+import { Drone } from "../database/models/drone";
+import { Medication } from "../database/models/medication";
+import { DroneStateEnum } from "../models/drone";
+
+export const validateToLoadMedication = async (req: Request, res: Response, next: NextFunction) => {
+    const { serialNumber, code } = req.body
+
+    try {
+        const drone = await Drone.findOne({ where:{ serialNumber }});
+
+        if (!drone) {
+            return res.status(400).json({ error: 'Invalid serial number' });
+        }
+    
+        const medication = await Medication.findOne({ where:{ code }});
+    
+        if (!medication) {
+            return res.status(400).json({ error: 'Invalid medication code' });
+        }
+    
+        if (drone?.batteryPercentage! < 25) {
+            return res.status(400).json({ error: 'The battery level is lower than 25%, please recharge' });
+        }
+    
+        if (drone?.state == DroneStateEnum.LOADING) {
+            return res.status(400).json({ error: 'Please wait until current loading completes' });
+        }
+    
+        req.body.drone = drone;
+        req.body.medication = medication;
+        next();
+    } catch (error) {
+        return res.status(500).json({ error: 'Something went wrong' });
+    }
+}
